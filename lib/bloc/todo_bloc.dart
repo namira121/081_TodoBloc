46a64e092555ed60ee3d23056dbd6c4a77a81237
namira121 <127211008+namira121@users.noreply.github.com>
2025-05-08 23:55:48 +0700

    jika state loaded, membuat salinan daftar todo dan kirim state baru berisi daftar todo yang sudah update

--- lib/bloc/todo_bloc.dart
+++ lib/bloc/todo_bloc.dart
@@ -6,6 +6,14 @@ part 'todo_state.dart';
 
 class TodoBloc extends Bloc<TodoEvent,TodoState>{
   TodoBloc(): super(TodoLoaded(todos: [], selectedDate: null)){
-    
+    on<TodoEventAdd>((event, emit){
+      final currenState = state;
+      if(currenState is TodoLoaded){
+        final List<Todo> updatedTodos = List.from(currenState.todos);
+        updatedTodos.add(Todo(title: event.title, date: event.date, isCompleted: false));
+        emit(TodoLoaded(todos: updatedTodos, selectedDate: currenState.selectedDate));
+      }
+    }
+    );
   }
 }
