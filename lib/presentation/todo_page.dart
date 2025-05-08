4e2c0cbc3dd7699acfdf1c92a6fa64ae8f689ab1
namira121 <127211008+namira121@users.noreply.github.com>
2025-05-09 00:50:34 +0700

    jika tanggal sudah dipilih, akan ditampilkan dengan hari/bulan/tahun. jika belum memilih tanggal akan mengeluarkan teks

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -23,7 +23,14 @@ class TodoPage extends StatelessWidget {
                       Text('Selected Date'),
                       BlocBuilder<TodoBloc, TodoState>(
                         builder: (context,state){
-                          
+                          if (state is TodoLoaded){
+                            if (state.selectedDate != null){
+                              return Text(
+                                '${state.selectedDate!.day}/${state.selectedDate!.month}/${state.selectedDate!.year}',
+                              );
+                            }
+                          }
+                          return Text('No date Selected');
                         }
                       )
                     ],
