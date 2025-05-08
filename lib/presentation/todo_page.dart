5e4dc589ce857d641fe2088e0cffcd8504c9dba0
namira121 <127211008+namira121@users.noreply.github.com>
2025-05-09 01:01:39 +0700

    ketika button ditekan akan menampilkan datepicker yang otomatis menampilkan tanggal sekarang, tanggal paling awal di tahun 2000 dan paling akhir di 2100

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -38,7 +38,14 @@ class TodoPage extends StatelessWidget {
                   SizedBox(width: 16.0,),
                   Expanded(
                     child: ElevatedButton(
-                      onPressed: onPressed, 
+                      onPressed: (){
+                        showDatePicker(
+                          context: context, 
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(2000), 
+                          lastDate: DateTime(2100)
+                        )
+                      }, 
                       child: child
                     )
                   )
