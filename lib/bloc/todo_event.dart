c204614d8b236a12fba9f325f57198959b12fb1c
namira121 <127211008+namira121@users.noreply.github.com>
2025-05-08 22:58:34 +0700

    membuat TodoSelectDate yang digunakan ketika memilih tanggal

--- lib/bloc/todo_event.dart
+++ lib/bloc/todo_event.dart
@@ -1,6 +1,6 @@
-sealed class TodoEvent{}
+sealed class TodoEvent {}
 
-final class TodoEventAdd extends TodoEvent{
+final class TodoEventAdd extends TodoEvent {
   final String title;
   final DateTime date;
   //untuk menambah todo
@@ -9,8 +9,17 @@ final class TodoEventAdd extends TodoEvent{
   TodoEventAdd({required this.title, required this.date});
 }
 
-final class TodoEventComplete extends TodoEvent { //untuk mengganti status
+final class TodoEventComplete extends TodoEvent {
+  //untuk mengganti status
   final int index; //untuk membaca array todo
 
-  TodoEventComplete({required this.index}); //constructor untuk membuat event mengganti status
+  TodoEventComplete({
+    required this.index,
+  }); //constructor untuk membuat event mengganti status
+}
+
+final class TodoSelectDate extends TodoEvent {
+  final DateTime date;
+
+  TodoSelectDate({required this.date});
 }
