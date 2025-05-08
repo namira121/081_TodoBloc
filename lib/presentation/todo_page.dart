1d108afd767627847bc0f3a9f0def2ccc4a1c3ce
namira121 <127211008+namira121@users.noreply.github.com>
2025-05-09 01:17:47 +0700

    menambahkan teks di button

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -52,7 +52,7 @@ class TodoPage extends StatelessWidget {
                           }
                         });
                       }, 
-                      child: child
+                      child: Text('Select Date'),
                     )
                   )
                 ],
