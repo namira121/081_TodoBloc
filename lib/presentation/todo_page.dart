9172dcd3c0aeb93054c011be825c5468c35cbb98
namira121 <127211008+namira121@users.noreply.github.com>
2025-05-09 02:18:27 +0700

    menampilkan hasil input tanggal dengan format hari/bulan/tahun

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -131,7 +131,11 @@ class TodoPage extends StatelessWidget {
                                         fontWeight: FontWeight.bold
                                       ),
                                     ),
-                                    SizedBox(height: 4.0,)
+                                    SizedBox(height: 4.0,),
+                                    Text(
+                                      '${todo.date.day}/${todo.date.month}/${todo.date.year}',
+                                      style: TextStyle(color: Colors.grey),
+                                    )
                                   ],
                                 )
                               ],
