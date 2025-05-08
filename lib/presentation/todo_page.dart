2aeaade6b1571876b19454187c04911cad42b458
namira121 <127211008+namira121@users.noreply.github.com>
2025-05-09 02:21:25 +0700

    jika status todo complete, warnanya akan menjadi hijau. Sebaliknya warnanya akan menjadi merah

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -135,6 +135,18 @@ class TodoPage extends StatelessWidget {
                                     Text(
                                       '${todo.date.day}/${todo.date.month}/${todo.date.year}',
                                       style: TextStyle(color: Colors.grey),
+                                    ),
+                                    SizedBox(height: 4.0,),
+                                    Text(
+                                      todo.isCompleted
+                                        ? 'Completed'
+                                        : 'Not Completed',
+                                      style: TextStyle(
+                                        color: 
+                                          todo.isCompleted
+                                            ? Colors.green
+                                            : Colors.red,
+                                      ),
                                     )
                                   ],
                                 )
