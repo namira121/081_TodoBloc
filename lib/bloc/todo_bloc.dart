5232d4a274159988deb2852819a251fb7e3668cb
namira121 <127211008+namira121@users.noreply.github.com>
2025-05-09 00:08:07 +0700

    menerima index dari todo yang ingin diubah statusnya. jika valid akan diubah status dan dikirim state baru

--- lib/bloc/todo_bloc.dart
+++ lib/bloc/todo_bloc.dart
@@ -22,5 +22,20 @@ class TodoBloc extends Bloc<TodoEvent,TodoState>{
         emit(TodoLoaded(todos: currentState.todos, selectedDate: event.date));
       }
     });
+
+    on<TodoEventComplete>((event, emit){
+      final currentState = state;
+      if (currentState is TodoLoaded){
+        final List<Todo> updatedTodos = List.from(currentState.todos);
+
+        if (event.index >= 0 && event.index < updatedTodos.length){
+          updatedTodos[event.index] = Todo(
+            title: updatedTodos[event.index].title,
+            date: updatedTodos[event.index].date,
+            isCompleted: !updatedTodos[event.index].isCompleted,
+          );
+        }
+      }
+    });
   }
 }
