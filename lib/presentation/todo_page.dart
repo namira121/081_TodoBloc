c8aad68cf392d9f9540ccaa98d5c6eb5a9e3c1a0
namira121 <127211008+namira121@users.noreply.github.com>
2025-05-09 00:18:03 +0700

    menambahkan flutter_bloc dan membuat statelesswidget TodoPage

---
+++ lib/presentation/todo_page.dart
@@ -0,0 +1,12 @@
+import 'package:tugasbloctodo_paml/bloc/todo_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class TodoPage extends StatelessWidget {
+  const TodoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
